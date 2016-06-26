804a6cf1df761ca987f646dc3bcee3f4d5755155
aashraya <aashraya_p@hotmail.com>
2016-06-26 15:06:57 -0500

    added player class file

--- blackjack.rb
+++ blackjack.rb
@@ -1,1 +1,7 @@
 #blackjack
+class Blackjack
+	def initialize
+	  puts "Welcome to Blackjack, a card game where you play against the dealer!"
+	  # @deck= Deck.new
+	  puts "Please enter your name: "
+	  @player= Player.new(gets.chomp)
---
+++ player.rb
@@ -0,0 +1,5 @@
+class Player
+	def intialize(name)
+		@name=name
+	end
+	attr_accessor :name
