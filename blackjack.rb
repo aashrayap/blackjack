4daf221e91bfbb047e685ce145e2e8b565113adf
aashraya <aashraya_p@hotmail.com>
2016-06-26 18:10:59 -0500

    tweaks

--- blackjack.rb
+++ blackjack.rb
@@ -8,12 +8,11 @@ class Blackjack
 	  @deck= Deck.new
 	  puts "Welcome to Blackjack, a card game where you play against the dealer!"
 	  # @deck= Deck.new
-	  puts "Please enter your name: "
-	  # @player= Player.new(gets.chomp)
+	  @player= Player.new('Aash')
 	end
 
 	def play
-		
+
 	end
 
 end
--- player.rb
+++ player.rb
@@ -1,6 +1,8 @@
 class Player
-	def intialize(name)
+	def intialize(name='Player1',hand=nil)
 		@name=name
+		@hand=hand
 	end
 	attr_accessor :name
+
 end
